3df833b5ddb77c47fd3227c9bc8b2a618aae09dd
Jorge Perez <jorgeperez@MacBook-Air.local>
2026-02-18 17:21:11 -0600

    feature: add get-iteme-by-category-id method

--- terraform/menu-api-resources.tf
+++ terraform/menu-api-resources.tf
@@ -150,6 +150,85 @@ resource "aws_api_gateway_integration_response" "options_menu" {
   }
 }
 
+# GET method for menu items by category (/menu/menu-items/items/by-category/{id})
+resource "aws_api_gateway_method" "get_menu_items_by_category_id" {
+  rest_api_id   = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id   = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "get_menu_items_by_category_id" {
+  rest_api_id = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method = aws_api_gateway_method.get_menu_items_by_category_id.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.ETag"                         = true
+    "method.response.header.X-Menu-Version"               = true
+    "method.response.header.X-Resource-Type"             = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_menu_items_by_category_id" {
+  rest_api_id = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method = aws_api_gateway_method.get_menu_items_by_category_id.http_method
+
+  integration_http_method = "POST"
+  type                   = "AWS_PROXY"
+  uri                    = aws_lambda_function.getMenuResource.invoke_arn
+}
+
+# CORS OPTIONS for menu items by category
+resource "aws_api_gateway_method" "options_menu_items_by_category_id" {
+  rest_api_id   = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id   = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_menu_items_by_category_id" {
+  rest_api_id = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method = aws_api_gateway_method.options_menu_items_by_category_id.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "options_menu_items_by_category_id" {
+  rest_api_id = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method = aws_api_gateway_method.options_menu_items_by_category_id.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_menu_items_by_category_id" {
+  rest_api_id = aws_api_gateway_rest_api.tacodelite_api.id
+  resource_id = aws_api_gateway_resource.menu_items_by_category_id.id
+  http_method = aws_api_gateway_method.options_menu_items_by_category_id.http_method
+  status_code = aws_api_gateway_method_response.options_menu_items_by_category_id.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,If-None-Match'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 # GET method for menu items (unified resource handler)
 resource "aws_api_gateway_method" "get_menu_items_unified" {
   rest_api_id   = aws_api_gateway_rest_api.tacodelite_api.id
